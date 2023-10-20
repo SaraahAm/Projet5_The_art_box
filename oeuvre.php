9e30261cc9fe5ad369244085a393fb24c0577b51
Sarah Ammari <sarah.ammari78@gmail.com>
2023-10-20 10:54:42 +0200

    Modification

--- oeuvre.php
+++ oeuvre.php
@@ -11,19 +11,19 @@
 <body>
 <?php include_once('header.php'); ?>
 <main>
-<?php include_once('oeuvres.php');
-    $id = $_GET['id'];
-    $oeuvre=$oeuvres[$id]?>
-    <article id="detail-oeuvre">
-        <div id="img-oeuvre">
-            <img src="<?php echo $oeuvre['image']?>" alt="<?php echo $oeuvre['nom']?>">
-        </div>
-        <div id="contenu-oeuvre">
-            <h1> <?php echo $oeuvre['nom'] ?></h1>
-            <p class="description"><?php echo $oeuvre['auteur'] ?></p>
-            <p class="description-complete"><?php echo $oeuvre['description']?></p>
-        </div>
-    </article>
+    <?php include_once('oeuvres.php');
+        $id = $_GET['id'];
+        $oeuvre=$oeuvres[$id]?>
+        <article id="detail-oeuvre">
+            <div id="img-oeuvre">
+                <img src="<?php echo $oeuvre['image']?>" alt="<?php echo $oeuvre['nom']?>">
+            </div>
+            <div id="contenu-oeuvre">
+                <h1> <?php echo $oeuvre['nom'] ?></h1>
+                <p class="description"><?php echo $oeuvre['auteur'] ?></p>
+                <p class="description-complete"><?php echo $oeuvre['description']?></p>
+            </div>
+        </article>
 </main>
 <?php include_once('footer.php'); ?>
 </body>
